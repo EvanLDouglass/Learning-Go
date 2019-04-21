d02513c0b4213bcdec1480c4bf11c6b27e356d5a
Evan <evandoug90@gmail.com>
2019-04-21 12:53:21 -0700

    File Restructure

---
+++ Udemy_LearnGo/LevelTwo/level_two.go
@@ -0,0 +1,10 @@
+// Simple exercises to get familiar with the Go programming language.
+// Exercises are from the Udemy course Learn How To Code Google's Go.
+// Author: Evan Douglass
+// Created: April 21 2019
+
+package main
+
+func main() {
+
+}
